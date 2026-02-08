7492494157062b225d4666bc97f473e11df3ffcd
VincentLeung <vleung1@gmail.com>
2026-02-08 09:10:31 -0500

    Add config for GitHub Pages

---
+++ config.js
@@ -0,0 +1,6 @@
+window.CONFIG = {
+  GOOGLE_CLIENT_ID: "1014590080509-mmt11vj1qq1cchn9qc0ejbs05vli80o2.apps.googleusercontent.com",
+  SHEET_ID: "1-9McubOjA0vXJSR4VbxePMBlUCde1LfAdJOQDnCj_KI",
+  SHEET_NAME: "ECM",
+  SHEET_RANGE: "A:E",
+};
