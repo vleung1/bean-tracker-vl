8ae10d6db345f54b0b01cb28d57211db458700d1
VincentLeung <vleung1@gmail.com>
2026-02-14 00:55:37 -0500

    Public and editor

--- editor/config.js
+++ editor/config.js
@@ -1,5 +1,5 @@
 window.CONFIG = {
-  API_URL: "",
-  API_TOKEN: "",
+  API_URL: "https://script.google.com/macros/s/AKfycbyhYLFMXxFe8mFXENoIDJYZGFb-JhHvQnRXg-tfGNzocj9bvwd9cQFywo8PLmh2Ous/exec",
+  API_TOKEN: "beantrackervl_x9K2mQ7pL4vN8tR1cD6yH3sF0wZ5uJ2aB7",
   APP_MODE: "editor"
 };
