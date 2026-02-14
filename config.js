9a0dc81ba72d4cdbd39a99fe64c421cdaf292344
VincentLeung <vleung1@gmail.com>
2026-02-14 01:06:11 -0500

    Fix public

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyxq3olLPr-qdAWnIaQkHN2_EzBIPyLTowupd7HbBiy1fpAoxVQYHAmEP8wUqPzoldF/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbxla9tj7ABak6BxM1q4gA3rmbhlUbFQezC3f07L2cPtLfzL2sZBl7joIGeQDAQhsxKcrA/exec",
   API_TOKEN: "beantrackervl_x9K2mQ7pL4vN8tR1cD6yH3sF0wZ5uJ2aB7",
   APP_MODE: "public"
 };
