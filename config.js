65415b090b9c18426c5cfe9fb813e174f8c0195e
VincentLeung <vleung1@gmail.com>
2026-02-13 23:10:24 -0500

    Configure Apps Script endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CONFIG = {
-  API_URL: "",
-  API_TOKEN: ""
+  API_URL: "https://script.google.com/macros/s/AKfycbyxq3olLPr-qdAWnIaQkHN2_EzBIPyLTowupd7HbBiy1fpAoxVQYHAmEP8wUqPzoldF/exec",
+  API_TOKEN: "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"
 };
